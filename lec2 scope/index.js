de968314a0773b7884bdd9a73afbe359e76798ba
Shivam Kumar <kumarys8922@gmail.com>
2026-07-21 23:56:04 +0530

    Initial commit

---
+++ lec2 scope/index.js
@@ -0,0 +1,22 @@
+var a=10;
+let b= 20;
+
+{
+    let b= 30;
+    var a= 40;
+    console.log(a,b)
+}
+
+console.log(a,b)
+
+var a= 10
+var c=60
+
+function fun(){
+   let a=20
+   var b=50
+    a++
+    c++
+    console.log(a,b,c)
+}
+console.log(a,b,c)
