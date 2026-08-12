e19f9fe2c6e22b5313198de592331330bee5d966
Shivam Kumar <kumarys8922@gmail.com>
2026-08-12 13:45:34 +0530

    array

---
+++ array/lec_1/array.js
@@ -0,0 +1,14 @@
+let students=[1,2,3,"Shivam",true,null];
+
+let posts=[];
+console.log(students);
+n=students.length;
+console.log(posts.length);
+console.log(students.length);
+
+let name =students[5];
+console.log(name);
+
+for(i=0;i<n; i++){
+    console.log("Shivam");
+}
